8771dcc2ac2c70c16c91012e60cec70d31ef48b1
katiepalumbo <palumbo.katie@gmail.com>
2022-04-12 23:16:52 +0200

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $posts = Post::with(['category'])->get(); 
+
+        return response()->json(
+            [
+                'results' => $posts,
+                'success' => true
+            ]
+        );
+
+    }
+
+}
